2d7781bfa7bc97b6848b83d7a96a7a6095458c15
Gerson <montenegrogerson@gmail.com>
2020-07-26 17:14:10 -0500

    chore: exceptions and control flow

---
+++ excepciones_y_control_de_flujo.py
@@ -0,0 +1,28 @@
+import pydash
+
+# EAFP style
+def busca_pais(paises, pais):
+    """
+    paises es un diccionario
+    pais es la llave
+    implementación principio EAFP
+    """
+    try:
+        return paises[pais]
+    except KeyError:
+        return None
+
+paises = ['Polombia', 'Chamozuela', 'Perú']
+# print (paises.index('Persú'))
+# result = pydash.collections.find(paises, lambda p: p == 'Pesrú')
+# print(result)
+# result = pydash.objects.find_key(paises, lambda p: p == 'Perú')
+# print(result)
+
+# LBYL style
+import pydash
+def buscaPais(paises, pais):
+    result = pydash.objects.find_key(paises, lambda p: p == pais)
+    return result
+
+print(f'LBYL style: {buscaPais(paises, "NombrePais")}')
